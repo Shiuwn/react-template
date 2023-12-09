9e91c26e05f2f59ce0fde0fb94d640a73dd17a35
shiu <wsh9075@163.com>
2023-12-09 10:24:09 +0800

    init

---
+++ uno.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  shortcuts: {
+    'flex-center': 'flex justify-center items-center',
+    'overlay': 'absolute top-0 bottom-0 left-0 right-0'
+  }
+})
